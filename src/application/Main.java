b01437869e2be7ee0636b7cafc11bd97e5827d9e
favour.adah <favour.adah@IC-CS233-9.uc.ucalgary.ca>
2022-07-26 10:09:23 -0600

    Restarted code repository because the project encountered a problem.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,16 +3,14 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root= new VBox();
-			root.getChildren().add(new Label("Favour's branch"));
+			BorderPane root= new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Favour's project");
